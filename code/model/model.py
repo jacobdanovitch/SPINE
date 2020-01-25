bd1b4214646f29c7429833856f507c8e6f00f13f
Jacob Danovitch <jacobgdt@gmail.com>
2020-01-25 13:51:48 -0500

    sequential spine

--- code/model/model.py
+++ code/model/model.py
@@ -49,3 +49,40 @@ class SPINEModel(torch.nn.Module):
 		return torch.sum(temp * temp) / self.hdim
 
 
+
+
+
+
+class SeqSPINEModel(SPINEModel):
+
+	def __init__(self, params):
+		super(SeqSPINEModel, self).__init__(params)
+		
+		# params
+		self.inp_dim = params['inp_dim']
+		self.hdim = params['hdim']
+		self.noise_level = params['noise_level']
+		self.getReconstructionLoss = nn.MSELoss()
+		self.rho_star = 1.0 - params['sparsity']
+		
+		# autoencoder
+		logging.info("Building model ")
+		self.linear1 = nn.Linear(self.inp_dim, self.hdim)
+		self.linear2 = nn.Linear(self.hdim, self.inp_dim)
+		
+
+	def forward(self, batch_x, batch_y):
+		
+		# forward
+		batch_size = batch_x.size(0)
+		linear1_out = self.linear1(batch_x)
+		h = linear1_out.clamp(min=0, max=1) # capped relu
+		out = self.linear2(h)
+
+		# different terms of the loss
+		reconstruction_loss = self.getReconstructionLoss(out, batch_y) # reconstruction loss
+		psl_loss = self._getPSLLoss(h, batch_size) 		# partial sparsity loss
+		asl_loss = self._getASLLoss(h)    	# average sparsity loss
+		total_loss = reconstruction_loss + psl_loss + asl_loss
+		
+		return out, h, total_loss, [reconstruction_loss,psl_loss, asl_loss]
