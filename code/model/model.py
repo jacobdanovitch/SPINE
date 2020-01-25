49efee1d0e63566e4306794f315c63ad5f91e880
Jacob Danovitch <jacobgdt@gmail.com>
2020-01-25 13:58:09 -0500

    forgot model

--- code/model/model.py
+++ code/model/model.py
@@ -1,5 +1,8 @@
 import torch
 from torch import nn
+
+from rae import Encoder, Decoder
+
 import logging
 logging.basicConfig(level=logging.INFO)
 
@@ -54,35 +57,20 @@ class SPINEModel(torch.nn.Module):
 
 
 class SeqSPINEModel(SPINEModel):
+    def __init__(self, params):
+        super(SeqSPINEModel, self).__init__(params)
+        
+        self.encoder = Encoder(params['seq_len'], params['hdim'], params['inp_dim'])
+        self.decoder = Decoder(params['seq_len'], params['inp_dim'], params['hdim'])
 
-	def __init__(self, params):
-		super(SeqSPINEModel, self).__init__(params)
-		
-		# params
-		self.inp_dim = params['inp_dim']
-		self.hdim = params['hdim']
-		self.noise_level = params['noise_level']
-		self.getReconstructionLoss = nn.MSELoss()
-		self.rho_star = 1.0 - params['sparsity']
-		
-		# autoencoder
-		logging.info("Building model ")
-		self.linear1 = nn.Linear(self.inp_dim, self.hdim)
-		self.linear2 = nn.Linear(self.hdim, self.inp_dim)
-		
+    def forward(self, x):
+        hidden = self.encoder(x)
+        reconstructed = self.decoder(hidden)
+        # different terms of the loss
+        reconstruction_loss = self.getReconstructionLoss(
+            reconstructed, x)  # reconstruction loss
+        psl_loss = self._getPSLLoss(hidden, hidden.size(0)) 		# partial sparsity loss
+        asl_loss = self._getASLLoss(hidden)    	# average sparsity loss
+        total_loss = reconstruction_loss + psl_loss + asl_loss
 
-	def forward(self, batch_x, batch_y):
-		
-		# forward
-		batch_size = batch_x.size(0)
-		linear1_out = self.linear1(batch_x)
-		h = linear1_out.clamp(min=0, max=1) # capped relu
-		out = self.linear2(h)
-
-		# different terms of the loss
-		reconstruction_loss = self.getReconstructionLoss(out, batch_y) # reconstruction loss
-		psl_loss = self._getPSLLoss(h, batch_size) 		# partial sparsity loss
-		asl_loss = self._getASLLoss(h)    	# average sparsity loss
-		total_loss = reconstruction_loss + psl_loss + asl_loss
-		
-		return out, h, total_loss, [reconstruction_loss,psl_loss, asl_loss]
+        return reconstructed, hidden, total_loss, [reconstruction_loss, psl_loss, asl_loss]
